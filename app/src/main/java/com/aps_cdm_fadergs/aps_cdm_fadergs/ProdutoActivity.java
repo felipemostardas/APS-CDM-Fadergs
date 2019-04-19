c081e0024a8b1f6cde769f0885a3369d84a3d68c
Luiz Junior <inf.gonzaga@gmail.com>
2019-04-19 17:30:57 -0300

    Commit 1004 - Produto

--- app/src/main/java/com/aps_cdm_fadergs/aps_cdm_fadergs/ProdutoActivity.java
+++ app/src/main/java/com/aps_cdm_fadergs/aps_cdm_fadergs/ProdutoActivity.java
@@ -2,12 +2,37 @@ package com.aps_cdm_fadergs.aps_cdm_fadergs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProdutoActivity extends AppCompatActivity {
+
+    private EditText etNome, etValor, etQuantidade;
+    private Button btnAdicionar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto);
+
+        etNome =(EditText) findViewById(R.id.etNome);
+        etValor =(EditText) findViewById(R.id.etValor);
+        etQuantidade =(EditText) findViewById(R.id.etNome);
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Salvar();
+            }
+        });
+
+
+    }
+
+    private void Salvar(){
+
+
     }
 }
