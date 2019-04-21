b3efc61caf562ef26174fbd6f43a6da3ec4cd9f9
Luiz Junior <inf.gonzaga@gmail.com>
2019-04-21 02:14:16 -0300

    Compra 1004

--- app/src/main/java/com/aps_cdm_fadergs/aps_cdm_fadergs/CompraActivity.java
+++ app/src/main/java/com/aps_cdm_fadergs/aps_cdm_fadergs/CompraActivity.java
@@ -1,31 +1,86 @@
 package com.aps_cdm_fadergs.aps_cdm_fadergs;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import java.util.List;
 
 
 public class CompraActivity extends AppCompatActivity {
+
+    ListView lvLista;
+    List<Produto> lista;
+    //    ArrayAdapter adapter;
+    AdapterCompra adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compra);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        lvLista = (ListView) findViewById(R.id.lvAnotacoes);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(
+                        CompraActivity.this, ProdutoActivity.class);
+                startActivity(intent);
             }
         });
+
+    }
+    private void carregarLista(){
+
+        lista = ProdutoDAO.listar(this);
+//        adapter = new ArrayAdapter(this,
+//                android.R.layout.simple_list_item_1, lista );
+        adapter = new AdapterCompra(this, lista);
+        lvLista.setAdapter(adapter);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregarLista();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lista_compra, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
